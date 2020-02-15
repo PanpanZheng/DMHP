004eb0d858b7f5319c117ffd4d7f8c5cc34e2b61
Panpan Zheng <panpanzheng@panpans-mbp.ddns.uark.edu>
2020-02-15 11:03:32 -0600

    add insiders.

--- DMHP.py
+++ DMHP.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("./")
 from DMHP_SMC import SMC
 
-for usr in ["AAB0398","AAC0610","AAC0668","AAC3270", "AAD2188"]:
+for usr in ["AAB0398","AAC0610","AAC0668","AAC3270", "AAD2188", "ACM2278", "CMP2946", "PLJ1771", "CDE1846", "MBG3183"]:
     print("***", usr)
     SMC(usr, 0.1, "./raw_input")
 
